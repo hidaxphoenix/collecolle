ec2c3ad67e4e6f4942a640238d2c5c9568ff32d0
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-03 19:55:07 +0900

    途中

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,6 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
     process resize_to_fit: [400, 400]
+    # process :quality => 90
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
