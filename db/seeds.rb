a2d58f5593279cad7d8878427c3df141ec500d3f
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-26 21:33:40 +0900

    add_tags_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tags = Tag.create([
+{name: "ホビー"},
+{name: "本"},
+{name: "音楽"},
+{name: "ファッション"},
+{name: "インテリア"},
+{name: "スポーツ"},
+{name: "家電・カメラ"},
+{name: "芸術"},
+{name: "その他"},
+])
