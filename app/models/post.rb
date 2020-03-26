32b0ab4a81eb9a207612b3c559f2a5d1b3768961
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-26 17:09:09 +0900

    add ajax

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source: :user
+  # has_many :liked_users, through: :likes, source: :user
   has_many :posttags,  dependent: :destroy
   has_many :tags, through: :posttags 
   mount_uploader :image, ImageUploader
