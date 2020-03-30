95700decfe95465ac37095fb3d82c76dfbcba582
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-30 16:32:46 +0900

    fix_user_controler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @posts = current_user.posts
+    @tags = Tag.all 
     # user.posts.page(params[:page]).per(5).order("created_at DESC")
   end
+
 end
