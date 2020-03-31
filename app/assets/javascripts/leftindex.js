f9633a0ef71c6b56684d4250cffdc9d04bc879f5
hidaxphoenix <zeecombu2115@gmail.com>
2020-03-31 14:21:58 +0900

    add_js_function

---
+++ app/assets/javascripts/leftindex.js
@@ -0,0 +1,75 @@
+$(function() {
+  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
+  $('.fas.fa-robot').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-book').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-music').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-tshirt').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-couch').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-running').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-tv').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-palette').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+  );
+  $('.fas.fa-feather-alt').hover(
+    function(){
+      $('.icontext').fadeIn();
+	      },
+	    function() {
+	      $('.icontext').fadeOut();
+	    }
+	);
+});
