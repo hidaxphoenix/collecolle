b92de01abde88e3076fe0ae5e36a3ba5b7c2ccbb
hidaxphoenix <zeecombu2115@gmail.com>
2020-04-02 21:42:00 +0900

    add_gettingtag-method_to all controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :getting_tag
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  private
+  def getting_tag
+    @tags = Tag.all 
   end
 end
