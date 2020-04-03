04e7bb54c62e68314d4d3afee50c737cdd1425d1
hidaxphoenix <zeecombu2115@gmail.com>
2020-04-03 20:26:02 +0900

    add_responsive_mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+before_action :responsive_mypage
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @posts = current_user.posts.page(params[:page]).per(16)
     @tags = Tag.all 
     # user.posts.page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  private
+  def responsive_mypage
+    sh = request.user_agent
+    if(sh.include?('Mobile')|| sh.include?('Android'))
+      request.variant = :mobile
+    end
   end
 
 end
